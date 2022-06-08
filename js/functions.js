9d79d5c4c90898393ebc8edab08956f7b755c27f
Vitória <vitoriapiloto477@gmail.com>
2022-06-08 11:37:24 -0300

    inserindo as principais paginas

---
+++ js/functions.js
@@ -0,0 +1,27 @@
+function focaEmail() {
+    let email = document.getElementById("eml");
+    email.style.border = 'none';
+    email.style.backgroundColor = 'black';
+    email.style.color = 'white';
+}
+
+function focaSenha() {
+    let senha = document.getElementById("pass");
+    senha.style.border = 'none';
+    senha.style.backgroundColor = 'black';
+    senha.style.color = 'white';
+}
+
+function resetaSenha() {
+    let senha = document.getElementById("pass");
+    senha.style.border = 'none';
+    senha.style.backgroundColor = 'white';
+    senha.style.color = 'black';
+}
+
+function resetaEmail() {
+    let email = document.getElementById("eml");
+    email.style.border = 'none';
+    email.style.backgroundColor = 'white';
+    email.style.color = 'black';
+}
